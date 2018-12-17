2d6e0aab1841818eb6f7fc0679ddf2501440fd66
animus apertus <37455165+ethantatum@users.noreply.github.com>
2018-12-17 13:37:01 -0700

    set up initial HTML, added Google fonts, started JS file with Firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAncTPNkOneyaJOH3rGj5SxMry-NdrWGJ0",
+        authDomain: "train-scheduler-b0d7e.firebaseapp.com",
+        databaseURL: "https://train-scheduler-b0d7e.firebaseio.com",
+        projectId: "train-scheduler-b0d7e",
+        storageBucket: "train-scheduler-b0d7e.appspot.com",
+        messagingSenderId: "814812132272"
+      };
+      firebase.initializeApp(config);
+
+    // Create a variable to reference the database
+    const database = firebase.database();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); // End of document.ready
