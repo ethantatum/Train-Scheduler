312ca574a5f8f067162fdc25b6b4c3f0c51d4423
animus apertus <37455165+ethantatum@users.noreply.github.com>
2018-12-17 14:09:44 -0700

    updated JavaScript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,9 +13,32 @@ $(document).ready(function() {
     // Create a variable to reference the database
     const database = firebase.database();
 
+    // Capture Button Click
+    $("#add-train").on("click", function(event) {
+        // Don't refresh the page!
+        event.preventDefault();
 
+        // Stores data for the most recent train
+        let trainName = $(`#name-input`).val().trim();
+        let destination = $(`#destination-input`).val().trim();
+        let firstTime = $(`#time-input`).val().trim();
+        let frequency = $(`#frequency-input`).val().trim();
 
+        console.log(trainName);
+        console.log(destination);
+        console.log(firstTime);
+        console.log(frequency);
+        
+        // Provides initial data to Firebase database
+        database.ref().push({
+            name: trainName,
+            destination: destination,
+            time: firstTime,
+            frequency: frequency,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        }); // Closes database.ref.push function
 
+    }); // Closes on-click function
 
 
 
