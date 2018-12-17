ece90f49fc1e8078f9a1580f2c43f27eafcfa3be
animus apertus <37455165+ethantatum@users.noreply.github.com>
2018-12-17 16:11:35 -0700

    working on moment.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,23 +22,63 @@ $(document).ready(function() {
         let trainName = $(`#name-input`).val().trim();
         let destination = $(`#destination-input`).val().trim();
         let firstTime = $(`#time-input`).val().trim();
+        let uniFirst = moment(`${firstTime}`, `x`);
         let frequency = $(`#frequency-input`).val().trim();
 
         console.log(trainName);
         console.log(destination);
         console.log(firstTime);
+        console.log(uniFirst);
         console.log(frequency);
         
         // Provides initial data to Firebase database
         database.ref().push({
             name: trainName,
             destination: destination,
-            time: firstTime,
             frequency: frequency,
+            time: firstTime,
             dateAdded: firebase.database.ServerValue.TIMESTAMP
         }); // Closes database.ref.push function
 
     }); // Closes on-click function
+
+    // Firebase watcher + initial loader
+    database.ref().on("child_added", function(snapshot) {
+        let sv = snapshot.val();
+
+        let pTrain = $(`<p>`).text(sv.name);
+        let pDest = $(`<p>`).text(sv.destination);
+        let pFreq = $(`<p>`).text(sv.frequency);
+
+        // Need to add sv.frequency to sv.time - moment.js?
+        let currentTime = moment().format(`x`);
+        console.log(currentTime);
+        let startUni = moment(`${sv.time}`, `x`);
+        console.log(startUni);
+        let difference = new moment().diff(sv.time, `m`);
+        //console.log(difference);
+        //let nextTrain = moment(`${sv.time}`).format(`HH:mm`).add(sv.frequency, `m`);
+        //.format(`HH:mm`);
+                                                
+        //console.log(nextTrain);
+        //let pTime = $(`<p>`).text(nextTrain);
+
+        // Need to subtract nextTrain time from current time to get minutes away
+        //let minutesAway = new moment().format(`HH:mm`) - (nextTrain);
+        //console.log(minutesAway);
+        //let pAway = $(`<p>`).text(minutesAway);
+
+
+        $(`#train-name`).append(pTrain);
+        $(`#destination`).append(pDest);
+        $(`#frequency`).append(pFreq);
+        //$(`#next-arrival`).append(pTime);
+        //$(`#minutes-away`).append(pAway);
+        // Create Error Handling
+        }, function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
+
+    }); // Closes on-child-added function 
 
 
 
